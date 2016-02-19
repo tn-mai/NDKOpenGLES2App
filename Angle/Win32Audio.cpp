2dfb6bf1b2d80af120d08777c605b10b8171e89a
tn_mai <rabitfiver@gmail.com>
2016-02-20 01:23:18 +0900

    Add the mock audio class for Angle Project.
    This implementation is because to compilable the code.
    It will implement in future, maybe.

---
+++ Angle/Win32Audio.cpp
@@ -0,0 +1,61 @@
+/** @file Win32Audio.cpp
+*/
+#include "../Shared/Audio.h"
+
+namespace Mai {
+
+/** The sound engine implementation.
+*/
+class AudioImpl: public AudioInterface {
+public:
+  AudioImpl();
+  virtual ~AudioImpl() { Finalize(); }
+  virtual bool Initialize();
+  virtual void Finalize();
+  virtual bool LoadSE(const char* id, const char* filename);
+  virtual void PrepareSE(const char* id);
+  virtual void PlaySE(const char* id);
+  virtual void PlayBGM(const char* filename);
+  virtual void StopBGM();
+  virtual void Clear();
+};
+
+AudioImpl::AudioImpl() {
+}
+/** Initialize sound controller object.
+*/
+bool AudioImpl::Initialize() {
+  return true;
+}
+
+void AudioImpl::Finalize() {
+}
+
+bool AudioImpl::LoadSE(const char* id, const char* filename) {
+  return false;
+}
+
+void AudioImpl::PrepareSE(const char* id)  {
+}
+
+void AudioImpl::PlaySE(const char* id) {
+}
+
+void AudioImpl::PlayBGM(const char* filename) {
+}
+
+void AudioImpl::StopBGM() {
+}
+
+void AudioImpl::Clear() {
+}
+
+/** Create the sound engine object.
+
+  @return the pointer to the interface of the sound engine implementation.
+*/
+AudioInterfacePtr CreateAudioEngine() {
+  return AudioInterfacePtr(new AudioImpl());
+}
+
+} // namespace Mai
