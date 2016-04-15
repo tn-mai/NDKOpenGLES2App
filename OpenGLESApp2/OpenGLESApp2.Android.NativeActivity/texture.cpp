1e5bce11fffbb54267e016f33602637f1ff0b245
tn_mai <rabitfiver@gmail.com>
2016-04-15 11:37:25 +0900

    add new line to the log.
    Fix that there is no new line in the texture log for Angle project.

--- OpenGLESApp2/OpenGLESApp2.Android.NativeActivity/texture.cpp
+++ OpenGLESApp2/OpenGLESApp2.Android.NativeActivity/texture.cpp
@@ -10,8 +10,8 @@
 #define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, "texture.cpp", __VA_ARGS__))
 #define LOGW(...) ((void)__android_log_print(ANDROID_LOG_WARN, "texture.cpp", __VA_ARGS__))
 #else
-#define LOGI(...) ((void)printf("texture.cpp" __VA_ARGS__))
-#define LOGW(...) ((void)printf("texture.cpp" __VA_ARGS__))
+#define LOGI(...) ((void)printf("texture.cpp" __VA_ARGS__), (void)printf("\n"))
+#define LOGW(...) ((void)printf("texture.cpp" __VA_ARGS__), (void)printf("\n"))
 #endif // __ANDROID__
 
 namespace Texture {
